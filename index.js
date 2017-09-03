7b84711b23b19f14c5bd65fb485282ef0ab43ffc
cab driver <cocktailsthecomic@gmail.com>
2017-09-03 23:01:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string){
   if (string.toUpperCase() === uppercase){
     return "YES INDEED!"
   };
-  if (string.toLowerCase() === lowercase){
+  if (lowercase.toLowerCase() === lowercase){
     return "I can't hear you!"
 
   };
