9a3ae902622bf56a0a3514e0434016a2d6b341ac
cab driver <cocktailsthecomic@gmail.com>
2017-09-03 22:47:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ function whisper(string){
   return string.toLowerCase();
 }
 function logShout(string){
-  return string.toUpperCase;
-  console.log(${string});
-}
+  toUpperCase(console.log(string))
+};
